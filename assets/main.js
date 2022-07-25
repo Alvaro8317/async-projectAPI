7179846d93b7ecdb6fcf6a5e6eb94894ab6eb381
Alvaro8317 <eduardo831_@hotmail.com>
2022-07-25 00:08:30 -0500

    Updates

--- assets/main.js
+++ assets/main.js
@@ -1,14 +1,33 @@
 containerImageDog = null || document.querySelector("#container-images");
 buttonRequest = document.querySelector(".searchApi .button button");
 const URLAPIDOG = "https://api.thedogapi.com/v1/images/search";
-buttonRequest.addEventListener("click", function(){showDoggy(URLAPIDOG)});
+buttonRequest.addEventListener("click", function(){showDoggyAwait(URLAPIDOG)});
+const img = document.querySelector("img");
+// /* Función hecha con promesas */
+// function showDoggy(URL) {
+//   fetch(URL)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       img.src = data[0].url;
+//       img.alt = data[0].id;
+//     });
+// }
 
-function showDoggy(URL) {
-  fetch(URL)
-    .then((res) => res.json())
-    .then((data) => {
-      const img = document.querySelector("img");
-      img.src = data[0].url;
-      img.alt = data[0].id;
-    });
+
+/* Función hecha con async - await */
+async function asyncData(URL){
+    const response = await fetch(URL);
+    const data = await response.json();
+    return data;
+}
+
+const showDoggyAwait = async(URL) => {
+    try{
+        const data = await asyncData(URL);
+        img.src = data[0].url;
+        img.alt = data[0].id;
+    }
+    catch (error){
+        console.log(error);
+    }
 }
